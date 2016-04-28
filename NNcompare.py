fc08383b045488bc9800b0504e1cdf7e8c7c471d
Obipls <o_louwaars@hotmail.com>
2016-04-28 17:31:47 +0200

    'dimensionality'

--- NNcompare.py
+++ NNcompare.py
@@ -9,30 +9,28 @@ from keras.preprocessing.text import Tokenizer
 
 def LSTMcomp(docTuple, maxTokens):
 
-    seqLength = 1
+	seqLength = 3000
 
-    a = np.atleast_3d((0,)+docTuple[0])
-    b = np.atleast_3d(docTuple[1]+(0,))
-    print(a.shape)
-    doc_a = Input(shape=[seqLength,maxTokens+1])
-    doc_b = Input(shape=[seqLength,maxTokens+1])
+	doc_a = Input(shape=(seqLength,maxTokens+1))
+	doc_b = Input(shape=(seqLength,maxTokens+1))
 
 
-    shared_lstm = LSTM(64)
+	shared_lstm = LSTM(64)
 
-    encoded_a = shared_lstm(doc_a)
-    encoded_b = shared_lstm(doc_b)
+	encoded_a = shared_lstm(doc_a)
+	encoded_b = shared_lstm(doc_b)
+
+	doc1 = docTuple[0]
+	doc2 = docTuple[1]
 
 
-    merged_vector = merge([encoded_a, encoded_b], mode='concat', concat_axis=-1)
+	merged_vector = merge([encoded_a, encoded_b], mode='concat', concat_axis=-1)
 
-    predictions = Dense(1,activation='sigmoid')(merged_vector)
+	predictions = Dense(1,activation='sigmoid')(merged_vector)
 
-    model = Model(input=[doc_a, doc_b], output=predictions)
+	model = Model(input=[doc_a, doc_b], output=predictions)
 
-    model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
+	model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
 
-    doc1= Reshape((10,3), input_shape=docTuple[0])
-    doc2= Reshape((10,3), input_shape=docTuple[1])
 
-    model.fit([a,b],1,nb_epoch=10)
+	model.fit([doc1,doc2],1,nb_epoch=10)
